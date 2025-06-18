f7fc58e882de93d195760d4e4ced7061b2af1b45
Vuk7912 <Vuk7912@users.noreply.github.com>
2025-06-18 00:43:14 +0000

    Add example service for testing

---
+++ src/example.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ExampleService {
+  add(a: number, b: number): number {
+    return a + b;
+  }
+
+  subtract(a: number, b: number): number {
+    return a - b;
+  }
+}
